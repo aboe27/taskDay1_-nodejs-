adf503b63ff7d9567d68faad4717a898f8ef7b0c
Windiana <krismanuyar@gmail.com>
2020-01-15 11:34:38 +0700

    Update task4.js

--- belajarday1/task4.js
+++ belajarday1/task4.js
@@ -1,5 +1,8 @@
 var name = ["john", "JACOB", "jinGleHeimer", "schmidt"];
 
+// jangan var w, lebih deskriptif lagi 
+//  indentasi masih jelek
+
 var w = name.map(
     w => w.substr(0,1).toUpperCase()+w.substr(1).toLowerCase()
     );
