5481e0568df7f5cfeca1054704a392e005fdfdb8
Windiana <krismanuyar@gmail.com>
2020-01-15 11:33:28 +0700

    Update task3.js

--- belajarday1/task3.js
+++ belajarday1/task3.js
@@ -8,6 +8,10 @@ var people = [
 ];
 
 //let friend = people.filter(people => people(friend=true));
+
+// nama callback di dalam filter nya jangan people lagi. dan secara flow sebenernya itu bagian dari people, jadi bisa dibuat kaya gini : 
+// var friends = people.filter(function (person) {
+    
 var friends = people.filter(function (people) {
     return people.friend==true;
 })
